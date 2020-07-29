0df804152e3f5b399aa0043c060779bdf06cdee8
Davis Raimon <davisraimon@gmail.com>
2020-07-29 09:35:40 +0530

    code update commit

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const inventory_routes = express.Router();
 const PORT = 4000;
-let Inventory = require('./inventory.model');
+let Inventory = require("./inventory.model");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,12 +21,30 @@ connection.once("open", function () {
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);
 });
-inventory_routes.route('/').get(function(req, res) {
-    Inventory.find(function(err, mst) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(mst);
-        }
-    });
+inventory_routes.route("/").get(function (req, res) {
+  Inventory.find(function (err, mst) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(mst);
+    }
+  });
+});
+inventory_routes.route("/:id").get(function (req, res) {
+  let id = req.params.id;
+  Inventory.findById(id, function (err, todo) {
+    res.json(todo);
+  });
+});
+
+inventory_routes.route('/add').post(function(req, res) {
+  console.log(req.body)
+  let inventory_item = new Inventory(req.body);
+  inventory_item.save()
+      .then(inventory_item => {
+          res.status(200).json({'todo': 'todo added successfully'});
+      })
+      .catch(err => {
+          res.status(400).send('adding new todo failed');
+      });
 });
